0f067d95f2ef442abbefaa13d09cf81890c24346
VitorCdSouza <vitorcorddesouza@gmail.com>
2026-09-16 16:15:29 -0300

    configuração CORS, com tratamento das origens configuradas no .env e requests no api.http

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { env } from './config/env';
 import { notFound } from './middlewares/notFound';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+app.use(cors({ origin: env.CORS_ORIGINS }));
 app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
